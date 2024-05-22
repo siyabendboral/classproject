df8419e6788d6a77ba42b57908e9edc40689634e
siyabendboral <boralsiyabend@gmail.com>
2024-05-22 14:04:42 -0500

    added module

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress-controller"
-
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
+resource "helm_release" "this" {
+  name       = "testapplication"
+  namespace =  "teststudy"
+  chart      = "${path.module}/charts/application"
+  wait       =  false
 }
